beba7388565c4af9bda306d04f8a25418b5886b7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-23 14:39:05 +0300

    navbar css

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import { Navbar } from './Navbar'
-import { Product } from './Product'
+import React from "react";
+import "../css/Home.css";
+import { Navbar } from "./Navbar";
+import { Product } from "./Product";
 export const Home = () => {
-    return (
-        <div>
-            <Navbar/>
-            <Product/>
-        </div>
-    )
-}
+  return (
+    <div className="wrapper">
+      <Navbar />
+      <Product />
+    </div>
+  );
+};
